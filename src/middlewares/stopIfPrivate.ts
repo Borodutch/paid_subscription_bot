de77b107e0f82b545de441b1da2a938e1c77ba2b
RMS <reishmax@gmail.com>
2021-07-01 08:23:31 +0700

    Minor fix

--- src/middlewares/stopIfPrivate.ts
+++ src/middlewares/stopIfPrivate.ts
@@ -1,7 +1,7 @@
 import { Context } from 'telegraf'
 
 export const stopIfPrivate = (ctx: Context, next: () => void) => {
-  sendStart  if (ctx.myChatMember.chat.type === 'private') {
+  if (ctx.myChatMember.chat.type === 'private') {
     return
   }
   return next()
