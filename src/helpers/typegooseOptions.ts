091bb419c393ddefec7bbfa173359832b52ae87f
Misha Suvorov <suvmih@mail.ru>
2021-08-04 11:29:13 +0300

    Refactor typegooseOptions.ts

--- src/helpers/typegooseOptions.ts
+++ src/helpers/typegooseOptions.ts
@@ -1,20 +1,7 @@
 import { Severity } from '@typegoose/typegoose'
-import mongoose from 'mongoose'
-
-interface TypegooseOptions {
-  existingMongoose?: mongoose.Mongoose
-  schemaOptions?: mongoose.SchemaOptions
-  existingConnection?: mongoose.Connection
-  options?: CustomOptions
-}
-interface CustomOptions {
-  customName?: string
-  automaticName?: boolean
-  allowMixed?: Severity
-  runSyncIndexes?: boolean
-}
+import { IModelOptions } from '@typegoose/typegoose/lib/types'
 
 export const typegooseOptions = {
   schemaOptions: { timestamps: true },
   options: { allowMixed: Severity.ALLOW },
-} as TypegooseOptions
+} as IModelOptions
